e3a1f7732182bc370d5ca75d360cba822920c20f
Michaelsh139 <michaelsh139@gmail.com>
2018-01-25 22:50:12 +0200

    Set up
    set up

---
+++ GGJ_DoubleDriver/Assets/Scripts/StearingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StearingScript : MonoBehaviour {
+
+    private float stearingRate; // On a scale of -2 to 2 when 0 is forward
+    public float 
+	
+    void Awake()
+    {
+        stearingRate = 0; // Forward
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("d") && stearingRate >= -2 && stearingRate < 2)
+        {
+
+        }
+	}
+}
